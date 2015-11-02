edafd55506ff5e771519a22cd53b8bfbd5a4f05a
mostasim <mostasim.10@gmail.com>
2015-11-02 21:07:08 +0600

    C file for me

---
+++ new.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+int main(){
+int i,j,k;
+i=1;
+j=22;
+char a[30];
+//scanf("%s",&a);
+a[5] ='Bari';
+printf("%c",a);
+
+//if(i ==10){
+//
+//    printf("You are a awesome bari !!!");
+//
+//}
+//else if(j ==20){
+//
+//printf("Cool Billah!!");
+//}else{
+//
+//
+//printf("Oil ur own machine dud!!!");
+//}
+//for(i=1;i<=10;++i){
+//
+//for(j=10;j>=i;j--){
+//
+//    printf(" ");
+//}
+//for(k=1;k<=i;k++){
+//
+//    printf("*");
+//}
+//printf("\n");
+//}
+//while(1){
+//
+//    printf("%d\n",i);
+//    if(i==100){
+//    break;
+//    }else{
+//    printf("Good Boy");
+//    }
+//    i++;
+//}
+//int arr[6];
+//arr[0]= 1;
+//arr[1]=2;
+//arr[2]=3;
+//arr[3]=4;
+//arr[4]=5;
+//arr[5]=6;
+//printf("%d\n%d\n%d",arr[0],arr[5],arr[4]);
+//char name[30];
+//name[4]='Bal';
+//printf("%s",name);
+
+
+
+}
+
